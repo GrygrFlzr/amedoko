75a7c079a714f8b01ba2451237216badf2828b50
GrygrFlzr <git@cybeast.dev>
2026-06-25 14:39:52 +0700

    merge svelte config into vite config
    kit 3 will remove the option
    https://github.com/sveltejs/kit/pull/16007 for details

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
+import { execSync } from 'node:child_process';
+import adapter from '@sveltejs/adapter-vercel';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			adapter: adapter(),
+			experimental: {
+				remoteFunctions: true,
+				explicitEnvironmentVariables: true
+			},
+			version: {
+				name: execSync('git rev-parse HEAD').toString().trim(),
+				pollInterval: 0
+			},
+			compilerOptions: {
+				runes: true,
+				modernAst: true,
+				experimental: {
+					async: true
+				}
+			}
+		})
+	],
 	oxc: {
 		assumptions: {
 			noDocumentAll: true
